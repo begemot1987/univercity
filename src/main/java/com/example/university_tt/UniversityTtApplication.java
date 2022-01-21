b7ed9f333f4e3f35a221516b8fb93ba0c0634ddc
Roman Polulikh <polilikh.r@gmail.com>
2022-01-21 16:42:01 +0200

    init commit

---
+++ src/main/java/com/example/university_tt/UniversityTtApplication.java
@@ -0,0 +1,13 @@
+package com.example.university_tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityTtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityTtApplication.class, args);
+    }
+
+}
